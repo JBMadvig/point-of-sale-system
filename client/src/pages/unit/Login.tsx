71f3529c855af6e731d71ec05e686aee9d7f7c95
JBMadvig <jonas.e.madvig@gmail.com>
2026-04-22 21:08:41 +0200

    feat(unit/login): Login now works

--- client/src/pages/unit/Login.tsx
+++ client/src/pages/unit/Login.tsx
@@ -2,33 +2,27 @@ import { Navigate } from 'react-router';
 import { useAuth } from '../../hooks/useAuth';
 import LoginForm from './components/LoginForm';
 import { SlidingPanelRoot } from '../../shared/components/SlidingPanel';
-import { ThemeToggle } from '../../shared/components/ThemeToggle';
-
+import { unitRoutes } from '../../shared/types/routes';
 
 function LoginMain() {
-	return (
-		<div className='flex flex-col items-center justify-center w-full gap-8 p-4 h-dvh'>
-			<img
-				className='aspect-video max-w-75'
-				src='/logo.webp'></img>
-			<LoginForm />
-			<div className='flex gap-3'>
-				<ThemeToggle />
-			</div>
-		</div>
-	);
+    return (
+        <div className='flex flex-col items-center justify-center w-full gap-8 p-4 h-dvh'>
+            <img className='aspect-video max-w-75' src='/logo.webp'></img>
+            <LoginForm />
+        </div>
+    );
 }
 
 export default function Login() {
-	const { user } = useAuth();
+    const { user } = useAuth();
 
-	if (user) {
-		return <Navigate to='/unit/landing' replace />;
-	}
+    if (user) {
+        return <Navigate to={unitRoutes.login} replace />;
+    }
 
-	return (
-		<SlidingPanelRoot>
-			<LoginMain />
-		</SlidingPanelRoot>
-	);
+    return (
+        <SlidingPanelRoot>
+            <LoginMain />
+        </SlidingPanelRoot>
+    );
 }
