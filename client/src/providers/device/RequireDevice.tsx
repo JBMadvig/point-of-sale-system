707332c4f23eb0e3008c8be9d03dd5c6ae726a40
JBMadvig <jonas.e.madvig@gmail.com>
2026-04-22 21:09:45 +0200

    refactor(requirePath): Now uses new route types as value

--- client/src/providers/device/RequireDevice.tsx
+++ client/src/providers/device/RequireDevice.tsx
@@ -1,19 +1,15 @@
 import { Navigate, Outlet } from 'react-router';
 import { useDevice } from '../../hooks/useDevice';
+import { deviceRoutes } from '../../shared/types/routes';
 
 export function RequireDevice() {
-	const { isActivated, isLoading } = useDevice();
+    const { isActivated, isLoading } = useDevice();
 
-	if (isLoading) return null; // or a spinner
+    if (isLoading) return null; // or a spinner
 
-	if (!isActivated) {
-		return (
-			<Navigate
-				to='/activate-device'
-				replace
-			/>
-		);
-	}
+    if (!isActivated) {
+        return <Navigate to={deviceRoutes.activate} replace />;
+    }
 
-	return <Outlet />;
+    return <Outlet />;
 }
